86967b152f0dd4f0cf8ef21077182bbda2427ddb
Oleksii <asallweb@gmail.com>
2023-04-03 14:55:40 +0200

    v1.09

--- dist/js/app.js
+++ dist/js/app.js
@@ -4545,17 +4545,33 @@
             swiperCard();
             window.addEventListener("resize", swiperCard);
         }
-        if (document.querySelector(".filter-list__slider")) new core(".filter-list__slider", {
-            modules: [ Navigation ],
-            observer: true,
-            observeParents: true,
-            centerInsufficientSlides: true,
-            slidesPerView: "auto",
-            spaceBetween: 10,
-            navigation: {
-                nextEl: ".filter-list__next-button"
+        if (document.querySelector(".filter-list__slider")) {
+            init = false;
+            swiper = false;
+            function swiperCard() {
+                if (window.innerWidth >= 768) {
+                    if (!init) {
+                        init = true;
+                        swiper = new core(".filter-list__slider", {
+                            modules: [ Navigation ],
+                            observer: true,
+                            observeParents: true,
+                            centerInsufficientSlides: true,
+                            slidesPerView: "auto",
+                            spaceBetween: 10,
+                            navigation: {
+                                nextEl: ".filter-list__next-button"
+                            }
+                        });
+                    }
+                } else if (init) {
+                    swiper.destroy();
+                    init = false;
+                }
             }
-        });
+            swiperCard();
+            window.addEventListener("resize", swiperCard);
+        }
         if (document.querySelector(".product-swiper__slider")) {
             const productSwiper = new core(".product-swiper__slider", {
                 modules: [ EffectFade, Pagination ],
@@ -6437,6 +6453,58 @@ PERFORMANCE OF THIS SOFTWARE.
     }
     const da = new DynamicAdapt("max");
     da.init();
+    const showFilterPopup = function(e) {
+        const attribute = this.getAttribute("data-popupId");
+        hideAllFilterPopup();
+        hideAll_activeClass();
+        if (this.classList.contains("_active")) hideCurrentFilterPopup(attribute); else showCurrentFilterPopup(attribute);
+        this.classList.toggle("_active");
+    };
+    const hideAllFilterPopup = function(e) {
+        let filterPopupList = document.querySelectorAll(".filter__popup");
+        for (var i = 0; i < filterPopupList.length; i++) filterPopupList[i].style.display = "none";
+    };
+    const closeFilterPopup = function(e) {
+        const element = e.target.closest(".filter__popup");
+        const button = e.target.closest(".filter-list__slide").getElementsByClassName("filter__btn")[0];
+        element.style.display = "none";
+        button.classList.remove("_active");
+    };
+    const hideCurrentFilterPopup = function(attribute) {
+        const element = document.getElementById(attribute);
+        element.style.display = "none";
+    };
+    const showCurrentFilterPopup = function(attribute) {
+        const element = document.getElementById(attribute);
+        if ("none" === element.style.display) element.style.display = "block";
+    };
+    const hideAll_activeClass = function(e) {
+        let filterBtn = document.querySelectorAll("[data-popupId]");
+        for (var i = 0; i < filterBtn.length; i++) filterBtn[i].classList.remove("_active");
+    };
+    let filterBtn = document.querySelectorAll("[data-popupId]");
+    for (var i = 0; i < filterBtn.length; i++) filterBtn[i].addEventListener("click", showFilterPopup, false);
+    let filterCloseBtn = document.querySelectorAll(".filter__popup-close");
+    for (i = 0; i < filterCloseBtn.length; i++) filterCloseBtn[i].addEventListener("click", closeFilterPopup, false);
+    document.addEventListener("click", (function(event) {
+        let isClickInsideBlock = event.target.closest(".filter__popup");
+        let isClickInsideFilterBtn = event.target.closest(".filter__btn");
+        if (!isClickInsideBlock && !isClickInsideFilterBtn) {
+            hideAllFilterPopup();
+            hideAll_activeClass();
+        }
+    }));
+    const toggleViewProduct = function(e) {
+        const attribute = this.getAttribute("data-view");
+        let toggleBtn = document.querySelectorAll("[data-view]");
+        for (var i = 0; i < toggleBtn.length; i++) toggleBtn[i].classList.remove("_active");
+        this.classList.toggle("_active");
+        document.documentElement.classList.remove("view-items-1");
+        document.documentElement.classList.remove("view-items-2");
+        document.documentElement.classList.add("view-items-" + attribute);
+    };
+    let toggleBtn = document.querySelectorAll("[data-view]");
+    for (i = 0; i < toggleBtn.length; i++) toggleBtn[i].addEventListener("click", toggleViewProduct, false);
     window["FLS"] = true;
     isWebp();
     menuInit();
