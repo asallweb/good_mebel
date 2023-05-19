fa25f78d48d3190aa2ebdc5cb59e0a09406e9e19
Oleksii <asallweb@gmail.com>
2023-05-19 14:38:42 +0200

    v1.34

--- dist/js/app.js
+++ dist/js/app.js
@@ -2049,6 +2049,25 @@
                 return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
             }
         };
+        function formQuantity() {
+            document.addEventListener("click", (function(e) {
+                let targetElement = e.target;
+                if (targetElement.closest("[data-quantity-plus]") || targetElement.closest("[data-quantity-minus]")) {
+                    const valueElement = targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]");
+                    let value = parseInt(valueElement.value);
+                    if (targetElement.hasAttribute("data-quantity-plus")) {
+                        value++;
+                        if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) value = valueElement.dataset.quantityMax;
+                    } else {
+                        --value;
+                        if (+valueElement.dataset.quantityMin) {
+                            if (+valueElement.dataset.quantityMin > value) value = valueElement.dataset.quantityMin;
+                        } else if (value < 1) value = 1;
+                    }
+                    targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]").value = value;
+                }
+            }));
+        }
         class SelectConstructor {
             constructor(props, data = null) {
                 let defaultConfig = {
@@ -6495,7 +6514,6 @@
                 observeParents: true,
                 slidesPerView: "auto",
                 spaceBetween: 34,
-                autoHeight: true,
                 preloadImages: false,
                 lazy: {
                     loadPrevNext: true,
@@ -6510,7 +6528,7 @@
                         spaceBetween: 20
                     },
                     768: {
-                        slidesPerView: 34
+                        spaceBetween: 34
                     }
                 }
             });
@@ -8643,10 +8661,18 @@ PERFORMANCE OF THIS SOFTWARE.
                 minutes.innerHTML = minutesLeft;
             }
         }));
+        const complectProducts = document.querySelectorAll(".complect-table__product");
+        complectProducts.forEach((function(complectProduct) {
+            const complectProductBtn = complectProduct.querySelector(".complect-table__product-toggle");
+            complectProductBtn.addEventListener("click", (event => {
+                complectProduct.classList.toggle("_opened");
+            }));
+        }));
         window["FLS"] = true;
         isWebp();
         menuInit();
         tabs();
+        formQuantity();
         spollers();
         showMore();
         headerScroll();
